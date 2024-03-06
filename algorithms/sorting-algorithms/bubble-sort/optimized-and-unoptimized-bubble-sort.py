0689357f0c96c53a351fc5bcd7f9ade86e7a64a4
Beregszászi István <beistvan@gmail.com>
2024-03-06 11:15:05 +0100

    Optimized And Unoptimized Bubble Sort

--- algorithms/sorting-algorithms/bubble-sort/optimized-and-unoptimized-bubble-sort.py
+++ algorithms/sorting-algorithms/bubble-sort/optimized-and-unoptimized-bubble-sort.py
@@ -31,3 +31,10 @@ def bubble_sort(arr):
 bubble_sort_unoptimized(nums.copy())
 bubble_sort(nums)
 print("POST SORT: {0}".format(nums))
+
+"""
+PRE SORT: [9, 8, 7, 6, 5, 4, 3, 2, 1]
+PRE-OPTIMIZED ITERATION COUNT: 72
+POST-OPTIMIZED ITERATION COUNT: 36
+POST SORT: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+"""
